b69d6b0b7be0a8c271db7aeef152464fcc874faa
Franklin Hurtado Salazar <137368806+Frankhs899@users.noreply.github.com>
2025-09-19 15:12:43 -0600

    feat CustomUser
    custom user model and its manager

--- users/models.py
+++ users/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
 
 # Create your models here.
+
+class CustomUserManager(BaseUserManager): 
+    def create_user(self, email, password=None, **extra_fields ): 
+        if not email: 
+            raise ValueError('El correo electrónico es un campo obligatorio')
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self,email, password=None, **extra_fields): 
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(max_length=255, unique=True)
+    first_name = models.CharField(max_length=255, null=True, blank=True)
+    last_name = models.CharField(max_length=255, null=True, blank=True)
+    username = models.CharField(max_length=255, null=True, blank=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
